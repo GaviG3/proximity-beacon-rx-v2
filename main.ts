66bfd5ebdbbfb7aed83bba9167173e22215ddd88
GaviG3 <132746834+GaviG3@users.noreply.github.com>
2023-05-06 15:04:00 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,16 +8,19 @@ radio.onReceivedString(function (receivedString) {
 let signal = 0
 radio.setGroup(1)
 basic.forever(function () {
+	
+})
+basic.forever(function () {
     if (radio.receivedPacket(RadioPacketProperty.SignalStrength) <= -128) {
         music.stopAllSounds()
-    } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) == -127 || radio.receivedPacket(RadioPacketProperty.SignalStrength) < -91) {
-        music.playTone(262, music.beat(BeatFraction.Half))
-        basic.pause(500)
-    } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) == -90 || radio.receivedPacket(RadioPacketProperty.SignalStrength) < -71) {
+    } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) <= -91) {
+        music.playTone(175, music.beat(BeatFraction.Half))
+        basic.pause(1000)
+    } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) <= -71) {
         music.playTone(349, music.beat(BeatFraction.Half))
         basic.pause(300)
-    } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) == -70 || radio.receivedPacket(RadioPacketProperty.SignalStrength) < -28) {
-        music.playTone(880, music.beat(BeatFraction.Half))
+    } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) <= -50) {
+        music.playTone(988, music.beat(BeatFraction.Half))
         basic.pause(75)
     }
 })
