f921aebef4a8ac7284f497523049abe64b3a3ee6
GaviG3 <132746834+GaviG3@users.noreply.github.com>
2023-05-06 15:34:57 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,18 @@ radio.onReceivedString(function (receivedString) {
     Math.map(signal, -95, -42, 0, 9),
     9
     )
+    if (signal < -128) {
+        music.stopAllSounds()
+    } else if (signal < -90) {
+        music.playTone(262, music.beat(BeatFraction.Half))
+        basic.pause(1000)
+    } else if (signal < -50) {
+        music.playTone(349, music.beat(BeatFraction.Half))
+        basic.pause(300)
+    } else if (signal < -28) {
+        music.playTone(988, music.beat(BeatFraction.Half))
+        basic.pause(75)
+    }
 })
 let signal = 0
 radio.setGroup(1)
@@ -11,16 +23,5 @@ basic.forever(function () {
 	
 })
 basic.forever(function () {
-    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) <= -128) {
-        music.stopAllSounds()
-    } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) <= -91) {
-        music.playTone(175, music.beat(BeatFraction.Half))
-        basic.pause(1000)
-    } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) <= -71) {
-        music.playTone(349, music.beat(BeatFraction.Half))
-        basic.pause(300)
-    } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) <= -50) {
-        music.playTone(988, music.beat(BeatFraction.Half))
-        basic.pause(75)
-    }
+	
 })
