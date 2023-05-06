9a8cdf77fa26098497859aba0a1657147a83384c
GaviG3 <132746834+GaviG3@users.noreply.github.com>
2023-05-06 16:32:32 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,17 +3,17 @@ radio.onReceivedString(function (receivedString) {
     if (signal < -128) {
         music.stopAllSounds()
         basic.showIcon(IconNames.No)
-    } else if (signal < -81) {
+    } else if (signal < -75) {
         music.playTone(262, music.beat(BeatFraction.Half))
         basic.pause(1000)
         basic.showIcon(IconNames.Square)
-    } else if (signal < -75) {
+    } else if (signal < -44) {
         music.playTone(349, music.beat(BeatFraction.Half))
         basic.pause(300)
         basic.showIcon(IconNames.Diamond)
     } else if (signal < -28) {
-        music.playTone(988, music.beat(BeatFraction.Half))
-        basic.pause(75)
+        music.playTone(988, music.beat(BeatFraction.Quarter))
+        basic.pause(50)
         basic.showIcon(IconNames.SmallDiamond)
     }
 })
